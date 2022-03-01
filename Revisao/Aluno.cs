fbf755d9ecbc465f9d3203e16568885d1667b7b0
Guimigliacci <guimigliacci@hotmail.com>
2022-03-01 20:25:35 -0300

    Efetuando a revisão

--- Revisao/Aluno.cs
+++ Revisao/Aluno.cs
@@ -2,6 +2,7 @@ namespace Revisao
 {
     public struct Aluno
     {
+        
         public string Nome { get; set; }
         public decimal Nota { get; set; }
     }
--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using   System;
 
 namespace Revisao
 {
@@ -8,15 +8,15 @@ namespace Revisao
         {
             Aluno[] alunos = new Aluno[5];
             var indiceAluno = 0;
-            string opcaoUsuario = ObterOpcaoUsuario();
+            string opcaoUsuario = obterOpcaodoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
             {
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        Console.WriteLine("Informe o nome do aluno:");
-                        var aluno = new Aluno();
+                        Console.WriteLine("Informe o nome do Aluno:");
+                        Aluno aluno = new Aluno();
                         aluno.Nome = Console.ReadLine();
 
                         Console.WriteLine("Informe a nota do aluno:");
@@ -33,72 +33,33 @@ namespace Revisao
                         alunos[indiceAluno] = aluno;
                         indiceAluno++;
 
+
                         break;
                     case "2":
-                        foreach (var a in alunos)
-                        {
-                            if (!string.IsNullOrEmpty(a.Nome))
-                            {
-                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
-                            }
-                        }
+                        // toDo: Listar Aluno
                         break;
                     case "3":
-                        decimal notaTotal = 0;
-                        var nrAlunos = 0;
-
-                        for (int i=0; i < alunos.Length; i++)
-                        {
-                            if (!string.IsNullOrEmpty(alunos[i].Nome))
-                            {
-                                notaTotal = notaTotal + alunos[i].Nota;
-                                nrAlunos++;
-                            }
-                        }
-
-                        var mediaGeral = notaTotal / nrAlunos;
-                        Conceito conceitoGeral;
-
-                        if (mediaGeral < 2)
-                        {
-                            conceitoGeral = Conceito.E;
-                        }
-                        else if (mediaGeral < 4)
-                        {
-                            conceitoGeral = Conceito.D;
-                        }
-                        else if (mediaGeral < 6)
-                        {
-                            conceitoGeral = Conceito.C;
-                        }
-                        else if (mediaGeral < 8)
-                        {
-                            conceitoGeral = Conceito.B;
-                        }
-                        else
-                        { 
-                            conceitoGeral = Conceito.A;
-                        }
-
-                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
-
+                        // toDo: Calcular média geral
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
+
+
                 }
 
-                opcaoUsuario = ObterOpcaoUsuario();
+                opcaoUsuario = obterOpcaodoUsuario();
+
             }
         }
 
-        private static string ObterOpcaoUsuario()
+        private static string obterOpcaodoUsuario()
         {
             Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
-            Console.WriteLine("1- Inserir novo aluno");
-            Console.WriteLine("2- Listar alunos");
-            Console.WriteLine("3- Calcular média geral");
-            Console.WriteLine("X- Sair");
+            Console.WriteLine("1 - Inserir novo Aluno");
+            Console.WriteLine("2 - Listar Aluno");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
@@ -107,3 +68,4 @@ namespace Revisao
         }
     }
 }
+
